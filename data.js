6a43cf9cf7dcce8d199edb8c8a538706ee58614d
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2024-02-15 12:54:00 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,36 @@
+const servicesData = [
+    {
+        title: "Frontend Development", 
+        category: "frontend", 
+        description: [
+            'I like to code things from scratch, and enjoy bringing ideas to life in the browser., the technnologies i use to build is HTML,CSS,Git' 
+        ]
+    },
+    {
+        title: "Backend Development",
+        category: "backend",
+        description: [
+            'Backend developers are responsible for building and maintaining the technology needed to power the frontend of websites and applications. This includes creating and managing databases, setting up servers, building APIs, i use the technologies like node.js Django,JavaScript,MongoDB for backend developent' // Description content here
+        ]
+    },
+    {
+        title: "Full stack Development",
+        category: "fullstack",
+        description: [
+            'Full stack development refers to the end-to-end application software development, including the front end and back end. The front end consists of the user interface, and the back end takes care of the business logic and application workflows. ,I use React,HTML,CSS,JavaScrpt,MangoDB,Django,Git ' // Description content here
+        ]
+    },
+    {
+        title: "Python",
+        category: "python",
+        description: [
+            'python projects be reason to vast development as the popular languane it allows in Programming,django as backend  ,heart of machine learning , and also good option to build aplications what ever you imagine'    ]
+    },
+    {
+        title: "Android Developement",
+        category: "androiddevelopement",
+        description: [
+            'Full stack development refers to the end-to-end application software development, including the front end and back end. The front end consists of the user interface, and the back end takes care of the business logic and application workflows. ,I use React,HTML,CSS,JavaScrpt,MangoDB,Django,Git ' // Description content here
+        ]
+    }
+];
